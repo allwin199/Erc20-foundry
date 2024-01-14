7d506a88486273f18aa3117799356b9777fb1cdb
worksofallwin <princeallwin199@gmail.com>
2024-01-14 16:25:20 +0530

    create ERC20 manually

---
+++ Erc20Details.md
@@ -0,0 +1,33 @@
+# ERC20
+
+-   EIP = Ethereum Improvement Proposal
+-   ERC = Ethereum Request for Comments
+
+-   ERC20 are tokens that are deployed on a chain using ERC20 token standard
+-   ERC20 is a smart contract that actually represents a token
+-   It is a token, it is a smart contract -> it is both
+
+## Applications of ERC20
+
+1. Governance Tokens
+2. Secure an underlying network
+3. Create a synthetic asset
+4. or anything else
+
+---
+
+## To Build ERC20
+
+-   Built a smart contract that follows ERC20 token standard
+
+### Key features and functions defined by the ERC-20 standard include:
+
+-   totalSupply: A function to get the total supply of tokens.
+
+-   balanceOf: A function to get the token balance of a specific Ethereum address.
+
+-   transfer: A function to transfer a specified number of tokens from the sender's address to another address.
+
+-   transferFrom: A function to allow a third party (approved by the token owner) to transfer tokens on behalf of the owner.
+
+-   approve and allowance: Functions to approve another address to spend tokens on behalf of the token owner.
---
+++ src/ManualToken.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract ManualToken {
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value)
+
+    mapping(address user => uint256 amount) private s_balances;
+    // holding tokens in ERC20 -> just mean we have some balance in some mapping
+
+    function name() public pure returns (string memory) {
+        return "NYCTOKEN";
+    }
+
+    function symbol() public pure returns (string memory) {
+        return "NYC";
+    }
+
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
+
+    function totalSupply() public pure returns (uint256) {
+        return 1000e18; // 1000 ether
+    }
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        return s_balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        uint256 callerBalance = s_balances[msg.sender];
+        if (callerBalance < _value) {
+            return false;
+        }
+        s_balances[msg.sender] -= _value;
+        s_balances[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+
+        // Whenver transfer fn is called
+        // msg.sender will be the caller
+        // He is transfering some balance from his account to the other
+        // To transfer, msg.sender balance should be greater than value
+    }
+}
